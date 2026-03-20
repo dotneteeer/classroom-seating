53d7c24ab56393f3c8a87a46d8d9823d83b2a55d
dotneteeer <447599489marat@gmail.com>
2026-03-20 17:33:58 +0300

    `vite.config.js` repo name updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Например: base: '/my-repo-name/'
 export default defineConfig({
   plugins: [react()],
-  base: '/ClassroomSeating/',
+  base: '/classroom-seating/',
 })
